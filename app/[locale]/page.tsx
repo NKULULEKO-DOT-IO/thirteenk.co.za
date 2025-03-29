1517a9e036f7f26efae59eabf0a2c1e1a6516451
Manuel-Sphe <sphesihlemanuel99@gmail.com>
2025-03-29 17:46:16 +0200

    Tailwind Confing

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -1,19 +1,22 @@
 import {useTranslations} from 'next-intl';
 import {Link} from '@/i18n/navigation';
 import ImageCard from "@/components/image-card";
+import '../globals.css'
 
 export default function HomePage() {
   const t = useTranslations('HomePage');
   return (
     <div>
-      <h1>{t('title')}</h1>
       <Link href="/about">{t('about')}</Link>
-      <ImageCard fileName={"/images/1.jpeg"}/>
-      <ImageCard fileName={"/images/1.jpeg"}/>
-      <ImageCard fileName={"/images/1.jpeg"}/>
-      <ImageCard fileName={"/images/1.jpeg"}/>
-      <ImageCard fileName={"/images/1.jpeg"}/>
-      <ImageCard fileName={"/images/1.jpeg"}/>
+      <h1>{t('title')}</h1>
+      <div className='container m-auto grid grid-cols-3 gap-4'>
+        <ImageCard fileName={"/images/1.jpeg"}/>
+        <ImageCard fileName={"/images/1.jpeg"}/>
+        <ImageCard fileName={"/images/1.jpeg"}/>
+        <ImageCard fileName={"/images/1.jpeg"}/>
+        <ImageCard fileName={"/images/1.jpeg"}/>
+        <ImageCard fileName={"/images/1.jpeg"}/>
+      </div>
     </div>
   );
 }
