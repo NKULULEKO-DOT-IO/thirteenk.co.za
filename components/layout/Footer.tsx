3caad68205d47219751b245c691cd55753a29cff
Vusani Makhomu <makhomuvusani23@gmail.com>
2025-04-20 23:56:06 +0200

    Move business plan to the top of page
    The more images which are uploaded, the more likely it is that the business plan won't get noticed since it's at the bottom. This change moved the plat to be at the header.

--- components/layout/Footer.tsx
+++ components/layout/Footer.tsx
@@ -1,5 +1,4 @@
 import { FC } from 'react';
-import { Link } from '@/i18n/navigation';
 import { FooterProps } from '@/types/layout/footer.types';
 import { useTranslations } from 'next-intl';
 
@@ -34,7 +33,6 @@ import { useTranslations } from 'next-intl';
 const Footer: FC<FooterProps> = ({
                                    className = '',
                                    copyrightText,
-                                   showBusinessPlanLink = true,
                                    children
                                  }) => {
   const t = useTranslations('Footer');
@@ -44,19 +42,6 @@ const Footer: FC<FooterProps> = ({
   return (
     <footer className={`py-8 border-t border-gray-100 ${className}`}>
       <div className="container mx-auto px-4">
-        {/* Business plan link */}
-        {showBusinessPlanLink && (
-          <div className="mt-8 text-center">
-            <Link
-              className="py-2 px-6 shadow-md rounded-md bg-blue-100 inline-block hover:bg-blue-200 transition-colors"
-              target="_blank"
-              rel="noopener noreferrer"
-              href="https://docs.google.com/document/d/1o0tT70ECWV11YB4oYH9RkkdllyLeO_ld8CEwGE3wlAg/edit?usp=sharing"
-            >
-              {t('businessPlan') || 'View Open Source Business Plan'}
-            </Link>
-          </div>
-        )}
 
         {/* Children content if provided */}
         {children && (
